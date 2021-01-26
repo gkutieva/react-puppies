ad8eaf519cac36f3b26e611fed48fb299627d36e
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-01-25 20:31:04 -0700

    created all routs and established new folders

--- models/puppySchema.js
+++ models/puppySchema.js
@@ -4,7 +4,7 @@ const puppySchema = new Schema(
   {
     name: { type: String, required: true },
     age: { type: Number, required: false },
-    breed: { type: Schema.Types.ObjectId, ref: "Breed" },
+    breed: { type: Schema.Types.ObjectId, ref: "Breed" }
   },
   {
     timestamps: true,
